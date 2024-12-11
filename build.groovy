37c950537d734b23e598d2eb7360099f00b25133
hellenmarashilian <hellenmarashilian@gmail.com>
2024-12-11 13:15:58 +0300

    Update build.groovy

--- build.groovy
+++ build.groovy
@@ -3,7 +3,8 @@ def call(MAP input_values){
   def default_values= [GIT_REPO_URL:GIT_REPO_URL, GIT_BRANCH:GIT_BRANCH, DOCKER_REGISTRY:DOCKER_REGISTRY, IMAGE_NAME:IMAGE_NAME, IMAGE_TAG:IMAGE_TAG, NEXUS_USERNAME:NEXUS_USERNAME, NEXUS_PASSWORD:NEXUS_PASSWORD]
   def map_to_apply=default_values+input_values
 
-node {
+pipeline {
+  stages{
     stage('Checkout Code') {
         echo 'Cloning repository...'
         git branch: GIT_BRANCH, url: GIT_REPO_URL
@@ -22,5 +23,6 @@ node {
             docker push ${DOCKER_REGISTRY}/${IMAGE_NAME}:${IMAGE_TAG}
         """
     }
+  }
 }  
 }
