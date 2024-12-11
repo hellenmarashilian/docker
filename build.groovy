c1926a361c41bf684a8106b4a8b60c16f224e8f5
hellenmarashilian <hellenmarashilian@gmail.com>
2024-12-11 15:04:42 +0300

    Update build.groovy

--- build.groovy
+++ build.groovy
@@ -11,40 +11,6 @@ def building(Map map_to_apply) {
        // NEXUS_PASSWORD: '2188'
     //]
     //def map_to_apply = default_values + input_values
+    echo "build.groovy executed with params: ${map_to_apply}"
 
-    pipeline {
-        agent any
-        stages {
-            stage('Checkout Code') {
-                steps {
-                    script {
-                        echo 'Cloning repository...'
-                        git branch: map_to_apply.GIT_BRANCH, url: map_to_apply.GIT_REPO_URL
-                    }
-                }
-            }
-
-            stage('Build Docker Image') {
-                steps {
-                    script {
-                        echo 'Building Docker image...'
-                        bat "docker build -t ${map_to_apply.IMAGE_NAME}:${map_to_apply.IMAGE_TAG} ./${map_to_apply.IMAGE_NAME}"
-                    }
-                }
-            }
-
-            stage('Push to Nexus Repository') {
-                steps {
-                    script {
-                        echo 'Pushing Docker image to Nexus...'
-                        bat """
-                            docker login ${map_to_apply.DOCKER_REGISTRY} -u ${map_to_apply.NEXUS_USERNAME} -p ${map_to_apply.NEXUS_PASSWORD}
-                            docker tag ${map_to_apply.IMAGE_NAME}:${map_to_apply.IMAGE_TAG} ${map_to_apply.DOCKER_REGISTRY}/${map_to_apply.IMAGE_NAME}:${map_to_apply.IMAGE_TAG}
-                            docker push ${map_to_apply.DOCKER_REGISTRY}/${map_to_apply.IMAGE_NAME}:${map_to_apply.IMAGE_TAG}
-                        """
-                    }
-                }
-            }
-        }
-    }
 }
