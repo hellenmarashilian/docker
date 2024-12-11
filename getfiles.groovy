4727f95052e58ee0c40936051462ae7a7638ba32
hellenmarashilian <hellenmarashilian@gmail.com>
2024-12-11 15:39:24 +0300

    Create getfiles.groovy

---
+++ getfiles.groovy
@@ -0,0 +1,15 @@
+def call(){
+  pipeline {
+    agent any
+    stages {
+        stage('Checkout Code') {
+            steps{
+                script{
+                echo 'Cloning repository...'
+                git branch: 'main', url: 'https://github.com/hellenmarashilian/docker.git'
+                }
+            }
+        }
+    }
+}
+}
