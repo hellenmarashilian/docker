cf252007d0c772b4c79b22b17af13151e7b6cede
hellenmarashilian <hellenmarashilian@gmail.com>
2024-12-11 13:34:28 +0300

    Update build.groovy

--- build.groovy
+++ build.groovy
@@ -1,12 +1,14 @@
+// build.groovy
+
 def call(Map input_values) {
     def default_values = [
-        git_repo_url: git_repo_url, 
-        git_branch: git_branch, 
-        docker_registry: docker_registry, 
-        image_name: image_name, 
-        image_tag: image_tag, 
-        nexus_username: nexus_username, 
-        nexus_password: nexus_password
+        GIT_REPO_URL: 'https://github.com/hellenmarashilian/docker.git',
+        GIT_BRANCH: 'main',
+        DOCKER_REGISTRY: '192.168.1.157:8083/repository/hellz-docker',
+        IMAGE_NAME: 'http_get',
+        IMAGE_TAG: 'latest',
+        NEXUS_USERNAME: 'admin',
+        NEXUS_PASSWORD: '2188'
     ]
     def map_to_apply = default_values + input_values
 
@@ -17,7 +19,7 @@ def call(Map input_values) {
                 steps {
                     script {
                         echo 'Cloning repository...'
-                        git branch: map_to_apply.git_branch, url: map_to_apply.git_repo_url
+                        git branch: map_to_apply.GIT_BRANCH, url: map_to_apply.GIT_REPO_URL
                     }
                 }
             }
@@ -26,7 +28,7 @@ def call(Map input_values) {
                 steps {
                     script {
                         echo 'Building Docker image...'
-                        bat "docker build -t ${map_to_apply.image_name}:${map_to_apply.image_tag} ./${map_to_apply.image_name}"
+                        bat "docker build -t ${map_to_apply.IMAGE_NAME}:${map_to_apply.IMAGE_TAG} ./${map_to_apply.IMAGE_NAME}"
                     }
                 }
             }
@@ -36,9 +38,9 @@ def call(Map input_values) {
                     script {
                         echo 'Pushing Docker image to Nexus...'
                         bat """
-                            docker login ${map_to_apply.docker_registry} -u ${map_to_apply.nexus_username} -p ${map_to_apply.nexus_password}
-                            docker tag ${map_to_apply.image_name}:${map_to_apply.image_tag} ${map_to_apply.docker_registry}/${map_to_apply.image_name}:${map_to_apply.image_tag}
-                            docker push ${map_to_apply.docker_registry}/${map_to_apply.image_name}:${map_to_apply.image_tag}
+                            docker login ${map_to_apply.DOCKER_REGISTRY} -u ${map_to_apply.NEXUS_USERNAME} -p ${map_to_apply.NEXUS_PASSWORD}
+                            docker tag ${map_to_apply.IMAGE_NAME}:${map_to_apply.IMAGE_TAG} ${map_to_apply.DOCKER_REGISTRY}/${map_to_apply.IMAGE_NAME}:${map_to_apply.IMAGE_TAG}
+                            docker push ${map_to_apply.DOCKER_REGISTRY}/${map_to_apply.IMAGE_NAME}:${map_to_apply.IMAGE_TAG}
                         """
                     }
                 }
