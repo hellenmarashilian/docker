1bab20a36dc1aa40930eab4ea4b1126ac7379d6f
hellenmarashilian <hellenmarashilian@gmail.com>
2024-12-11 13:47:33 +0300

    Update build.groovy

--- build.groovy
+++ build.groovy
@@ -1,16 +1,16 @@
 // build.groovy
 
-def call(Map input_values) {
-    def default_values = [
-        GIT_REPO_URL: 'https://github.com/hellenmarashilian/docker.git',
-        GIT_BRANCH: 'main',
-        DOCKER_REGISTRY: '192.168.1.157:8083/repository/hellz-docker',
-        IMAGE_NAME: 'http_get',
-        IMAGE_TAG: 'latest',
-        NEXUS_USERNAME: 'admin',
-        NEXUS_PASSWORD: '2188'
-    ]
-    def map_to_apply = default_values + input_values
+def call(Map map_to_apply) {
+//    def default_values = [
+ //       GIT_REPO_URL: 'https://github.com/hellenmarashilian/docker.git',
+  //      GIT_BRANCH: 'main',
+   //     DOCKER_REGISTRY: '192.168.1.157:8083/repository/hellz-docker',
+    //    IMAGE_NAME: 'http_get',
+     //   IMAGE_TAG: 'latest',
+      //  NEXUS_USERNAME: 'admin',
+       // NEXUS_PASSWORD: '2188'
+    //]
+    //def map_to_apply = default_values + input_values
 
     pipeline {
         agent any
