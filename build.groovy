50f9b1c6de792e30b552561eb6e8948b08bc4daa
hellenmarashilian <hellenmarashilian@gmail.com>
2024-12-11 13:18:05 +0300

    Update build.groovy

--- build.groovy
+++ build.groovy
@@ -1,4 +1,4 @@
-def call(MAP input_values){
+def call(Map input_values){
 
   def default_values= [GIT_REPO_URL:GIT_REPO_URL, GIT_BRANCH:GIT_BRANCH, DOCKER_REGISTRY:DOCKER_REGISTRY, IMAGE_NAME:IMAGE_NAME, IMAGE_TAG:IMAGE_TAG, NEXUS_USERNAME:NEXUS_USERNAME, NEXUS_PASSWORD:NEXUS_PASSWORD]
   def map_to_apply=default_values+input_values
